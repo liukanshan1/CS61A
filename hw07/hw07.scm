ff5b56231551c6da73161f1b39e4103b2af18236
LiuKanshan <1269595029@qq.com>
2022-05-17 08:04:20 -0700

    done with hw07

--- hw07/hw07.scm
+++ hw07/hw07.scm
@@ -1,11 +1,35 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+    (car (cdr s))
+)
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+    (car (cdr (cdr s)))
+)
 
-(define (ordered? s) 'YOUR-CODE-HERE)
+(define (ordered? s) 
+    (if (null? s) 
+        #t
+        (if (null? (cdr s))
+            #t
+            (if (not (> (car s) (cadr s)))
+                (ordered? (cdr s))
+                #f
+            )
+        )
+
+    )
+)
 
 (define (square x) (* x x))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (if (= exp 1)
+        base
+        (if (even? exp)
+            (pow (square base) (/ exp 2))
+            (* base (pow (square base) (/ (- exp 1) 2)))
+        )
+    )
+)
