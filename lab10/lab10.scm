5807878d622c947b8c78b080fbbb843b4981eac5
LiuKanshan <1269595029@qq.com>
2022-05-17 06:56:31 -0700

    done with lab10

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,9 +1,25 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+     (cond ((< num1 num2) (- 1))
+        ((= num1 num2) 0)
+        (else 1)
+     )
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (x) (+ x num))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
 
-(define lst 'YOUR-CODE-HERE)
+(define lst 
+    (list (list 1) 2 (list 3 4) 5)
+)
 
-(define (remove item lst) 'YOUR-CODE-HERE)
+(define (remove item lst) 
+    (if (null? lst) 
+        ()
+        (car (list (filter (lambda (x) (not (= x item))) lst)))
+    )
+)
