3b4e0a2757596a9dc6e2ba41ffdf0450dc460db6
LiuKanshan <1269595029@qq.com>
2022-05-17 07:13:05 -0700

    upload hw07

---
+++ hw07/hw07.scm
@@ -0,0 +1,11 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
+(define (caddr s) 'YOUR-CODE-HERE)
+
+(define (ordered? s) 'YOUR-CODE-HERE)
+
+(define (square x) (* x x))
+
+(define (pow base exp) 'YOUR-CODE-HERE)
