33f7bd570782500e0c07997184bcd983628833f7
LiuKanshan <1269595029@qq.com>
2022-05-21 08:39:10 -0700

    done with hw09

--- hw09/hw09.py
+++ hw09/hw09.py
@@ -19,7 +19,7 @@ def roman_numerals(text):
     >>> roman_numerals("she loves ALL editors equally.")
     []
     """
-    return re.findall(__________, text)
+    return re.findall(r"\b[IVXLCDM]+\b", text)
 
 
 import re
@@ -43,7 +43,7 @@ def cs_classes(post):
     >>> cs_classes("What are some good CS upper division courses? I was thinking about CS 161 or CS 169a")
     True
     """
-    return bool(re.search(__________, post))
+    return bool(re.search(r"\b(CS|cs)\s?\d+[ABCabc]?\b", post))
 
 
 import re
@@ -60,7 +60,8 @@ def match_time(text):
     >>> match_time("At 2:00 I pinged 127.0.0.1:80.")
     ['2:00']
     """
-    return re.findall(__________, text)
+    return re.findall(r"(?:2[1-3]|[0-1]\d|\d):[0-5]\d(?:AM|PM)?", text)
+
 
 
 import re
@@ -79,7 +80,7 @@ def area_codes(text):
     >>> area_codes("no matches for 12 3456 7890 or 09876-54321")
     []
     """
-    return re.findall(__________, text)
+    return re.findall(r"\(?(\d{3})\)?\s?\d{3}\s?\d{4}(?:\D|\b)", text)
 
 
 def most_common_code(text):
@@ -97,4 +98,14 @@ def most_common_code(text):
     >>> most_common_code(input_text)
     '123'
     """
-    "*** YOUR CODE HERE ***"
+    codes = area_codes(text)
+    maxCount = 0
+    maxCode = ""
+    for code in codes:
+        count = codes.count(code)
+        if count > maxCount:
+            maxCount = count
+            maxCode = code
+    return maxCode
+
+
--- hw09/tests/wwrm.py
+++ hw09/tests/wwrm.py
@@ -5,7 +5,7 @@ test = {
     {
       'cases': [
         {
-          'answer': 'd074f67b3b777b03ecc810123a1fbae1',
+          'answer': 'Any 6-digit hexadecimal color code, like #fdb515',
           'choices': [
             'Any 6-digit hexadecimal color code, like #fdb515',
             'A hexadecimal color code that starts with letters and ends with numbers, like #gg1234',
@@ -13,12 +13,12 @@ test = {
             'A hexadecimal color code with 3 letters and 3 numbers'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': '#[a-f0-9]{6}'
         },
         {
-          'answer': '4ed2cbe877f71b60b3007b745a7641b6',
+          'answer': 'Only fizzbuzz, fizz, and buzz',
           'choices': [
             'Only fizzbuzz',
             'Only fizz',
@@ -27,12 +27,12 @@ test = {
             'Only fizzbuzz or buzz'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': '(fizz(buzz|)|buzz)'
         },
         {
-          'answer': '459cd95194a7e36b8f3d9bbd1b11e44f',
+          'answer': 'Signed or unsigned numbers like +1000, -1.5, .051',
           'choices': [
             'Signed or unsigned numbers like +1000, -1.5, .051',
             'Only unsigned numbers like 0.051',
@@ -40,7 +40,7 @@ test = {
             'Only signed or unsigned integers like +1000, -33'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': '`[-+]?\\d*\\.?\\d+`'
         }
