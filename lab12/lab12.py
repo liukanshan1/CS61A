0e6a5fa07c7c74c009cc830b1eb17d00a081f8e5
LiuKanshan <1269595029@qq.com>
2022-05-22 06:22:46 -0700

    done with lab12

--- lab12/lab12.py
+++ lab12/lab12.py
@@ -13,4 +13,4 @@ def calculator_ops(calc_str):
     >>> calculator_ops("(* 2)")
     []
     """
-    return re.findall(__________, calc_str)
+    return re.findall(r"\(([+\-*/]\s\d+\s\d+)\)", calc_str)
--- lab12/tests/ebnf-grammar-wwpd.py
+++ lab12/tests/ebnf-grammar-wwpd.py
@@ -5,7 +5,7 @@ test = {
     {
       'cases': [
         {
-          'answer': '0b94adbb0a0cc0b6d57ec431dbbe8411',
+          'answer': 'Each of the operands can itself be a Calculator expression.',
           'choices': [
             'The subtraction operator requires at least one argument.',
             'The division operator requires at least two arguments.',
@@ -13,12 +13,12 @@ test = {
             'Variables can be defined and used as operands.'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'Which aspects of the Calculator language are supported by this grammar?'
         },
         {
-          'answer': 'ad713cfc496e3310a6b081b1e086fe06',
+          'answer': '(1 + 2)',
           'choices': [
             '(+ 1 2)',
             '(+)',
@@ -28,12 +28,12 @@ test = {
             '(+ 1 (+ 2 3))'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'Which of the following expressions would NOT be parsable according to that BNF?'
         },
         {
-          'answer': 'e491d25a2d6dfc085d2bb42c134dec05',
+          'answer': 'OPERATOR: "+" | "-" | "*" | "/"',
           'choices': [
             'start: calc_expr',
             '?calc_expr: NUMBER | calc_op',
@@ -41,12 +41,12 @@ test = {
             'OPERATOR: "+" | "-" | "*" | "/"'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'Which line of the BNF should we modify to add support for calculations using a modulus operator, like (% 15 5)?'
         },
         {
-          'answer': '1da69a2e56bee7f35cbc1272ec1b6723',
+          'answer': 'No, this grammar gives enough information for parsing a Calculator expression, but it does not provide enough information to evaluate it.',
           'choices': [
             'Yes, if we feed this grammar into a program that understands BNF grammars, it will be able to parse Calculator expressions and output the result.',
             'Yes, but only if we feed this grammar into a program that was written in the Calculator language itself.',
@@ -54,7 +54,7 @@ test = {
             'No, this grammar does not provide enough information for the parsing or evaluation step, it is useful mostly as documentation.'
           ],
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False,
           'question': 'Does the BNF grammar provide enough information to create a working interpreter for this version of the Calculator language?'
         }
