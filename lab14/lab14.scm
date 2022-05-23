87ffb6c3ff21d8a05e8e0b2c172c1e75f943268b
LiuKanshan <1269595029@qq.com>
2022-05-23 06:43:03 -0700

    upload

---
+++ lab14/lab14.scm
@@ -0,0 +1,3 @@
+(define (split-at lst n) 'YOUR-CODE-HERE)
+
+(define (compose-all funcs) 'YOUR-CODE-HERE)
---
+++ lab14/lab14.sql
@@ -0,0 +1,23 @@
+create table pizzas as
+  select "Pizzahhh" as name, 12 as open, 15 as close union
+  select "La Val's"        , 11        , 22          union
+  select "Sliver"          , 11        , 20          union
+  select "Cheeseboard"     , 16        , 23          union
+  select "Emilia's"        , 13        , 18;
+
+create table meals as
+  select "breakfast" as meal, 11 as time union
+  select "lunch"            , 13         union
+  select "dinner"           , 19         union
+  select "snack"            , 22;
+
+
+-- Pizza places that open before 1pm in alphabetical order
+create table opening as
+SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+-- Two meals at the same place
+create table double as
+SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
